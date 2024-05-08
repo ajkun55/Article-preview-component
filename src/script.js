754beed4c0e1e6613e1c60d1004d242788de31c9
ajkun55 <81357643+ajkun55@users.noreply.github.com>
2024-05-08 19:11:04 +0800

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,23 @@
+
+const modal = document.getElementById("modal-content");
+
+// Get the button that opens the modal
+const btn = document.getElementById("modal-button");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it   
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
